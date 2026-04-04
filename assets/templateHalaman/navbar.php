18be82ab4214f85421008af2fd8b340f51190515
Kyiotaro <kyiotaro7@gmail.com>
2026-04-04 14:36:19 +0700

    Search bar v0.1

--- assets/templateHalaman/navbar.php
+++ assets/templateHalaman/navbar.php
@@ -3,7 +3,10 @@
     <a href="../landingpage/landingpage.php"><img src="../assets/Foto/brand/logo.png" alt="Logo"></a>
     <h1>Permata Biru Nusantara</h1>
   </div>
-
+  <div class="search-bar">
+    <input type="text" placeholder="Cari artikel...">
+    <button><img src="../assets/Foto/ui/search-icon.png" alt="Search"></button>
+  </div>
   <nav class="nav-links">
     <a href="../landingpage/landingpage.php#terpopuler">Artikel</a>
     <a href="../landingpage/landingpage.php#terpopuler">Terpopuler</a>
